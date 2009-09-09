6c367607cbde1f2178d57e2a076f11559c1a760b
Luke Amdor <luke@absoluterubbish.net>
2009-09-09 15:51:24 -0500

    Returning a List[String] on StringUtils.toCollection  # this breaks non-recompiled clients which expect a Array[String]

--- src/main/scala/StringUtils.scala
+++ src/main/scala/StringUtils.scala
@@ -2,5 +2,5 @@ object StringUtils {
   def joiner(strings: List[String], seperator: String) = strings.mkString(seperator)
   def joiner(strings: List[String]): String = joiner(strings, " ")
 
-  def toCollection(s: String) = s.split(" ")
+  def toCollection(s: String) = s.split(" ").toList
 }
