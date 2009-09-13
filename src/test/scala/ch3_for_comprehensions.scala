7eb73e373198b3659bbaaa7a929098f7de0ca31c
Luke Amdor <luke@absoluterubbish.net>
2009-09-13 12:25:59 -0500

    Added learning specs for for comprehensions

---
+++ src/test/scala/ch3_for_comprehensions.scala
@@ -0,0 +1,53 @@
+import org.specs._
+
+object ForComprehensionSpecification extends Specification {
+  val dogBreeds = List("Doberman", "Yorkshire Terrier",
+                       "Collie", "Labrador",
+                       "Scottish Terrier", "Poodle",
+                       "Great Dane")
+
+  "the for comprehension" should {
+    "iterate through a list" >> {
+      var result = List[String]()
+      for (breed <- dogBreeds) {
+        result ::= breed
+        println(breed)
+      }
+      result must beEqualTo(dogBreeds.reverse)
+    }
+
+    "filter a list" >> {
+      var result = List[String]()
+      for (breed <- dogBreeds
+           if breed.contains("Terrier"))
+        result ::= breed
+
+      result must beEqualTo(List("Scottish Terrier", "Yorkshire Terrier"))
+    }
+
+    "handle multiple filters" >> {
+      var result = List[String]()
+      for (breed <- dogBreeds
+           if breed.contains("Terrier");
+           if !breed.startsWith("Yorkshire"))
+        result ::= breed
+      result must beEqualTo(List("Scottish Terrier"))
+    }
+
+    "yield the entry creating a list (instead of appending)" >> {
+      val filteredBreeds = for { breed <- dogBreeds
+                                if breed.contains("Terrier")
+                                if !breed.startsWith("Yorkshire")
+                              }  yield breed
+      filteredBreeds must beEqualTo(List("Scottish Terrier"))
+    }
+
+    "allow variables in it's scope (expanded scope)" >> {
+      val filteredBreeds = for { breed <- dogBreeds
+                                upcasedBreed = breed.toUpperCase
+                                if breed.contains("Terrier")
+                              } yield upcasedBreed
+      filteredBreeds must beEqualTo(List("YORKSHIRE TERRIER", "SCOTTISH TERRIER"))
+    }
+  }
+}
