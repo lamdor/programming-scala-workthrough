52eee97a16fa1de043371a04bbcf55a5abf51102
Luke Amdor <luke@absoluterubbish.net>
2009-09-13 11:59:06 -0500

    Added more learning specs showing more examples without parens

--- src/test/scala/ch3_methods_without_parens.scala
+++ src/test/scala/ch3_methods_without_parens.scala
@@ -4,6 +4,16 @@ import org.specs.runner.JUnit4
 class OperatorPrecedenceSpecificationTest extends JUnit4(OperatorPrecedenceSpecification)
 
 object OperatorPrecedenceSpecification extends Specification {
+  List(1,2,3).size must_== 3
+
+  def isEven(n: Int) = (n % 2) == 0
+
+  isEven(4) must beTrue
+  isEven(3) must beFalse
+
+  List(1,2,3,4,5,6) filter isEven must_== List(2,4,6)
+  List(1,2,3,4) filter isEven foreach println
+
   (2.0 * 4.0 / 5.0 * 5.0) must_== 8.0
 
   val list = List(1,2,3)
@@ -11,4 +21,5 @@ object OperatorPrecedenceSpecification extends Specification {
 
   List(4,5,6) ++ list must_== List(4,5,6,1,2,3)
   'a' :: List(4,5,6) ++ list must_== List('a',4,5,6,1,2,3)
+  'a' :: 'b' :: list must_== List('a', 'b', 1, 2, 3)
 }
