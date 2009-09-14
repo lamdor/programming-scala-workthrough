b034994250e138a1ae726f3be9493a73428a6a71
Luke Amdor <luke@absoluterubbish.net>
2009-09-14 11:30:07 -0500

    Making trait abstract override definitions a little more like the rest

--- src/main/scala/ui.scala
+++ src/main/scala/ui.scala
@@ -15,7 +15,7 @@ class Button(label: String) extends Widget with Clickable {
 }
 
 trait ObservableClicks extends Clickable with Subject {
-  abstract override def click() {
+  abstract override def click() = {
     super.click
     notifyObservers
   }
@@ -32,7 +32,7 @@ trait VetoableClicks extends Clickable {
   val maxAllowed = 1
   private var count = 0
 
-  abstract override def click() {
+  abstract override def click() = {
     if (count < maxAllowed) {
       super.click()
       count += 1
