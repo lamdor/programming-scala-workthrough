e8ae922f3d64f6dfbae5f4a2535e3f9468538783
Luke Amdor <luke@absoluterubbish.net>
2009-09-14 08:54:36 -0500

    Added learning spec to show that traits are instantiated in a left to right manner

--- src/test/scala/ch4_traits.scala
+++ src/test/scala/ch4_traits.scala
@@ -84,6 +84,32 @@ object TraitsSpecification extends Specification {
       for (i <- 1 to 3) button.click
       buttonClickObserver.count must_== 3
     }
+
+    "be constructed in a left to right manner" in {
+      trait T1 {
+        println("  in T1: x = " + x)
+        val x = 1
+        println("  in T1: x = " + x)
+      }
+      trait T2 {
+        println("  in T2: y = " + y)
+        val y = "T2"
+        println("  in T2: y = " + y)
+      }
+      class Base12 {
+        println("  in Base12: b = " + b)
+        val b = "Base12"
+        println("  in Base12: b = " + b)
+      }
+      class C12 extends Base12 with T1 with T2 {
+        println("  in C12: c = " + c)
+        val c = "C12"
+        println("  in C12: c = " + c)
+      }
+      println("Creating C12:")
+      new C12
+      println("After Creating C12")
+    }
   }
 
   "VetoableClicks" should {
