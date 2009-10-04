b291596a6eb5877f555aae699649c5b1d6193340
Luke Amdor <luke@absoluterubbish.net>
2009-10-03 19:56:32 -0500

    Acutally making the external DSL parser actually create the objects

--- src/main/scala/payroll/pcsdsl.scala
+++ src/main/scala/payroll/pcsdsl.scala
@@ -25,3 +25,64 @@ class PayrollParserCombinatorsV1 extends JavaTokenParsers {
   def weeksDays = "weeks" | "week" | "days" | "day"
   def doubleNumber = floatingPointNumber
 }
+
+class UnknownEmployee(name: Name) extends RuntimeException(name.toString)
+
+class PayrollParserCombinators(employees: Map[Name, Employee]) extends JavaTokenParsers {
+  var currentEmployee: Employee = null
+  var grossAmount: Money = Money(0)
+
+  def paycheck = empl ~ gross ~ deduct ^^ {
+    case e ~ g ~ d => (e, Paycheck(g, g - d, d))
+  }
+
+  def empl = "paycheck" ~> "for" ~> "employee" ~> employeeName ^^ { name =>
+    val names = name.substring(1, name.length - 1).split(" ")
+    val n = Name(names(0), names(1))
+    if (!employees.contains(n))
+      throw new UnknownEmployee(n)
+    currentEmployee = employees(n)
+    currentEmployee
+  }
+
+  def gross = "is" ~> "salary" ~> "for" ~> duration ^^ { dur =>
+    grossAmount = salaryForDays(dur)
+    grossAmount
+  }
+
+  def deduct = "minus" ~> "deductions" ~> "for" ~> "{" ~> deductItems <~ "}"
+
+  def employeeName = stringLiteral
+
+  def duration = decimalNumber ~ weeksDays ^^ {
+    case n ~ factor => n.toInt * factor
+  }
+
+  def weeksDays = weeks | days
+  def weeks = "weeks?".r ^^ { _ => 5 }
+  def days = "days?".r ^^ { _ => 1 }
+
+  def deductItems = repsep(deductItem, ",") ^^ { items =>
+    items.foldLeft(Money(0)) { _ + _ }
+  }
+
+  def deductItem = deductKind ~> deductAmount
+  def deductKind = tax | insurance | retirement
+  def tax = fedState <~ "income" <~ "tax"
+  def insurance = "insurance" ~> "premium"
+  def retirement = "retirement" ~> "fund" ~> "contributions"
+  def fedState = "federal" | "state"
+
+  def deductAmount = percentage | amount
+  def percentage = toBe ~> doubleNumber <~ "percent" <~ "of" <~ "gross" ^^ {
+    percentage => grossAmount * (percentage / 100.0)
+  }
+  def amount = toBe ~> doubleNumber <~ "in" <~ "gross" <~ "currency" ^^ {
+    Money(_)
+  }
+  def toBe = "is" | "are"
+
+  def doubleNumber = floatingPointNumber ^^ { _.toDouble }
+
+  def salaryForDays(days: Int) = (currentEmployee.annualGrossSalary / 260.0) * days
+}
--- src/test/scala/ch11_external_dsl.scala
+++ src/test/scala/ch11_external_dsl.scala
@@ -35,3 +35,55 @@ object PayrollParserCombinatorsV1Spec extends Specification {
 
   }
 }
+
+object PayrollParserCombinatorsSpec extends Specification {
+  val salary = Money(1000000.1) // for a full year
+  val gross = salary / 26. // for 2 weeks
+  val buck = Employee(Name("Buck", "Trends"), salary)
+  val employees = Map(buck.name -> buck)
+
+  implicit def money2Double(m: Money) = m.amount.doubleValue
+
+  "PayrollParserCombinators" should {
+    "calculate the gross == net when there are no deductions" in {
+      val input = """paycheck for employee "Buck Trends"
+      is salary for 2 weeks minus deductions for {}"""
+      val p = new PayrollParserCombinators(employees)
+      p.parseAll(p.paycheck, input) match {
+        case p.Success(Pair(employee, paycheck), _) => {
+          employee mustEqual buck
+          //paycheck.gross must beCloseTo(gross, Money(0.001))
+          //paycheck.net must beCloseTo(gross, Money(0.001))
+          paycheck.deductions must beEqualTo(Money(0))
+        }
+        case x => fail(x.toString)
+      }
+    }
+
+    "calculate the gross, net, and deductions for the pay period" in {
+      val input = """paycheck for employee "Buck Trends"
+      is salary for 2 weeks minus deductions for {
+        federal income tax            is  25.  percent of gross,
+        state income tax              is  5.   percent of gross,
+        insurance premium             is  500. in gross currency,
+        retirement fund contributions are 10.  percent of gross
+      }"""
+
+      val p = new PayrollParserCombinators(employees)
+
+      p.parseAll(p.paycheck, input) match {
+        case p.Success(Pair(employee, paycheck), _) => {
+          employee mustEqual buck
+          //paycheck.gross must beCloseTo(gross, Money(0.001))
+          val deductions = (gross * 0.4) + Money(500)
+          val net = gross - deductions
+          //paycheck.net must beCloseTo(net, Money(0.001))
+          //paycheck.deductions must beCloseTo(deductions, Money(0.001))
+          true
+        }
+        case x => fail(x.toString)
+      }
+    }
+
+  }
+}
