31451cc243afc40c0e477b5304e53ec9aa55a608
Luke Amdor <luke@absoluterubbish.net>
2009-10-26 17:46:25 -0500

    Fixed typo

--- project/build/ProgrammingScala.scala
+++ project/build/ProgrammingScala.scala
@@ -4,5 +4,5 @@ class ProgrammingScalaProject(info: ProjectInfo) extends DefaultProject(info) {
   val slf4j = "org.slf4j" % "slf4j-jdk14" % "1.5.2"
   val mina = "org.apache.mina" % "mina-core" % "2.0.0-M6"
   val specs = "org.scala-tools.testing" % "specs" % "1.6.0" % "test"
-  val junit = "junit" % "junit" % "4.7" % "testn"
+  val junit = "junit" % "junit" % "4.7" % "test"
 }
